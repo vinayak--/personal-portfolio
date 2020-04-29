914a6597d04d4423adfe326cf53322e5d4e68432
Vinayak Jethe <jethevinayak@gmail.com>
2020-04-28 21:23:38 -0700

    Project working

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Creating blog database models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Blog
 # Create your views here.
+def all_blogs(request):
+    #if you want all objects
+    #blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-date') #If you want sorts the blogs as per date and returns the top 5 blogs then add  [:5] in the end
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk = blog_id)
+    return render(request, 'blog/detail.html', {'blog':blog})
