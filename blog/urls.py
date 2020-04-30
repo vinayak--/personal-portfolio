0b98d2999053f0cb312de9560b466adb791c7352
Vinayak Jethe <jethevinayak@gmail.com>
2020-04-29 20:18:23 -0700

    Final touch

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('',views.all_blogs, name = 'all blogs'),
+    path('',views.all_blogs, name = 'all_blogs'),
     path('<int:blog_id>/',views.detail, name = 'detail'),
 ]
